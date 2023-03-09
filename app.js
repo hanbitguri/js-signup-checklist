b4eac1bd4c40e97258f5420d268bec8b25fcb63a
전한빛 <asjask123@gmail.com>
2023-03-09 20:34:26 +0900

    📸 fix a logic

--- app.js
+++ app.js
@@ -1,61 +1,83 @@
-const button = document.querySelector('.signup-button')
-const checkboxAll = document.querySelector('.choosebox-all')
-const checkboxEssential = document.querySelectorAll('.choosebox-essential')
-const checkbox = document.querySelector('.choosebox')
+const allCheck = document.querySelector('.all-check')
+const essentialCheck = document.querySelectorAll('.essential-check')
+const optionalCheck = document.querySelector('.optional-check')
 
+const checks = [essentialCheck[0],essentialCheck[1],optionalCheck]
 
-checkboxEssential[0].addEventListener('click',()=>{
-    checkboxEssential[0].toggleAttribute('checked')
-    reValue()
-    setAllcheck()
-})
-checkboxEssential[1].addEventListener('click',()=>{
-    checkboxEssential[1].toggleAttribute('checked')
-    reValue()
-    setAllcheck()
-})
-checkbox.addEventListener('click',()=>{
-    checkbox.toggleAttribute('checked')
-    setAllcheck()
-})
+const signUpButton = document.querySelector('.signup-button')
 
-checkboxAll.addEventListener('click',allClickHandler)
+const allCheckedHandler = () =>{
+    const allChecked =
+    allCheck.classList.contains('is-active')
+    essentialCheck[0].classList.contains('is-active') &&
+    essentialCheck[1].classList.contains('is-active') &&
+    optionalCheck.classList.contains('is-active')
 
-function allClickHandler(){
-        checkboxAll.toggleAttribute('checked')
-        if(checkboxEssential[0].getAttribute('checked')===null && checkboxEssential[1].getAttribute('checked')===null && checkbox.getAttribute('checked') === null){
-            checkboxEssential.forEach((item)=>{
-                item.setAttribute('checked','')
-            })
-            checkbox.setAttribute('checked','')
-            reValue()
-        }else if(checkboxEssential[0].getAttribute('checked')!=null && checkboxEssential[1].getAttribute('checked')!=null && checkbox.getAttribute('checked') != null){
-            checkboxEssential.forEach((item)=>{
-                item.removeAttribute('checked')
-            })
-            checkbox.removeAttribute('checked')
-            reValue()
-        }
-        
-    
+    if(allChecked){
+        allCheck.classList.remove('is-active')
+        essentialCheck[0].classList.remove('is-active') &&
+        essentialCheck[1].classList.remove('is-active') &&
+        optionalCheck.classList.remove('is-active')
+    }
+}
+
+const allCheckHandler=()=>{
+    const allChecked =
+    allCheck.classList.contains('is-active') &&
+    essentialCheck[0].classList.contains('is-active') &&
+    essentialCheck[1].classList.contains('is-active') &&
+    optionalCheck.classList.contains('is-active')
+
+    if(allChecked){
+        allCheck.classList.remove('is-active')
+        essentialCheck[0].classList.remove('is-active') 
+        essentialCheck[1].classList.remove('is-active')
+        optionalCheck.classList.remove('is-active')
+        activeSignUp()
+        return
+    }
+    allCheck.classList.toggle('is-active')
+
     //
+    const stuff = checks.filter((check)=>{
+        return !(check.classList.contains('is-active'))
+    })
+    stuff.forEach((stuff)=>{
+        stuff.classList.add('is-active')
+    })
+    activeSignUp()
+}
+const essentialCheckHandler=(i)=>{
+    essentialCheck[i].classList.toggle('is-active')
+    activeSignUp()
+    allCheckTrigger()
+}
+const optionalCheckHandler=()=>{
+    optionalCheck.classList.toggle('is-active')
+    activeSignUp()
+    allCheckTrigger()
+}
+
+allCheck.addEventListener('click',allCheckHandler)
+essentialCheck[0].addEventListener('click',()=>essentialCheckHandler(0))
+essentialCheck[1].addEventListener('click',()=>essentialCheckHandler(1))
+optionalCheck.addEventListener('click',optionalCheckHandler)
+
+const activeSignUp = () =>{
+    const buttonIsActive = 
+    essentialCheck[0].classList.contains('is-active') &&
+    essentialCheck[1].classList.contains('is-active')
     
+    if(buttonIsActive) signUpButton.classList.add('is-active')
+    if(!buttonIsActive) signUpButton.classList.remove('is-active')
 }
+const allCheckTrigger = () =>{
+    const allChecked =
+    essentialCheck[0].classList.contains('is-active') &&
+    essentialCheck[1].classList.contains('is-active') &&
+    optionalCheck.classList.contains('is-active')
 
-function setAllcheck(){
-    if(checkboxEssential[0].getAttribute('checked') !=null && checkboxEssential[1].getAttribute('checked') !=null && checkbox.getAttribute('checked') != null){
-        checkboxAll.setAttribute('checked','')
-    }else{
-        checkboxAll.removeAttribute('checked','')
-    }
-}
-
-function reValue(){
-    if(checkboxEssential[0].getAttribute('checked') !=null && checkboxEssential[1].getAttribute('checked') !=null){
-        button.classList.add('is-active')
-        
-    }else{
-        button.classList.remove('is-active')
-    }
+    if(allChecked) allCheck.classList.add('is-active')
+    if(!allChecked) allCheck.classList.remove('is-active')
 }
 
